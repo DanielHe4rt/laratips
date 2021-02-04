3d73483bd69ef21fcf944c2d7fc4ee8a97ac7dd7
danielhe4rt <idanielreiss@gmail.com>
2021-02-03 21:42:52 -0300

    feat: translation

---
+++ resources/lang/pt_BR/view.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+    'welcome' => [
+        'documentation' => 'Documentation',
+        'documentationDescription' => 'Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.',
+        'news' => 'Laravel News',
+        'newsDescription' => 'Laravel News is a community driven portal and newsletter aggregating all of the latest and most important news in the Laravel ecosystem, including new package releases and tutorials.',
+        'laracasts' => 'Laracasts',
+        'laracastsDescription' => 'Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.',
+        'ecosystem' => 'Vibrant Ecosystem',
+        'ecosystemDescription' => 'Laravel\'s robust library of first-party tools and libraries, such as <a href="https://forge.laravel.com" class="underline">Forge</a>, <a href="https://vapor.laravel.com" class="underline">Vapor</a>, <a href="https://nova.laravel.com" class="underline">Nova</a>, and <a href="https://envoyer.io" class="underline">Envoyer</a> help you take your projects to the next level. Pair them with powerful open source libraries like <a href="https://laravel.com/docs/billing" class="underline">Cashier</a>, <a href="https://laravel.com/docs/dusk" class="underline">Dusk</a>, <a href="https://laravel.com/docs/broadcasting" class="underline">Echo</a>, <a href="https://laravel.com/docs/horizon" class="underline">Horizon</a>, <a href="https://laravel.com/docs/sanctum" class="underline">Sanctum</a>, <a href="https://laravel.com/docs/telescope" class="underline">Telescope</a>, and more.'
+    ]
+];
